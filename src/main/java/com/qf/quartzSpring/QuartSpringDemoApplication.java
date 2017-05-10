26e126a33badb67462fa7ae062ef4e588d435288
DESKTOP-825PJRQ\puhui <13554254100@163.com>
2017-05-10 20:46:37 +0800

    demo commit

---
+++ src/main/java/com/qf/quartzSpring/QuartSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package com.qf.quartzSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartSpringDemoApplication.class, args);
+	}
+}
